1c49614f58268a5d307465abea44efa8a7c4eb8a
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-09-07 19:28:37 +0300

    Update 1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 namespace myspace {
-    void printNTimes(const char *str, int n = 10) {
+    void printNTimes(const char* str, int n = 10) {
         for(int i = 0; i < n; i++) {
             std::cout << str;
         }
