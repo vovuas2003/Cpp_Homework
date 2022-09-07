6dc20194ce43ad2964184306931aff2b73d42682
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-09-07 19:07:36 +0300

    Update 2.cpp

--- 1/2.cpp
+++ 1/2.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
-void cube(int& a) {
+int cubeV(int a) {
+    return a * a * a;
+}
+
+void cubeR(int& a) {
     a = a * a * a;
 }
 
 int main() {
     int x = 2;
     std::cout << x << std::endl;
-    cube(x);
+    cubeR(x);
+    std::cout << x << std::endl;
+    std::cout << std::endl;
+    x = 2;
+    std::cout << x << std::endl;
+    x = cubeV(x);
     std::cout << x << std::endl;
 }
