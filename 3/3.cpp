d9c6ddac3fb79a1ee23e23280ddb8bee824ac507
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-10-12 22:12:43 +0300

    Create 3.cpp

---
+++ 3/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string_view>
+#include <string>
+
+std::string operator*(std::string a, int n) {
+    std::string otv = "";
+    for(int i = 0; i < n; i++) {
+        otv += a;
+    }
+    return otv;
+}
+
+int main() {
+    std::string a = "PT!";
+    std::cout << a * 5 << std::endl;
+    return 0;
+}
