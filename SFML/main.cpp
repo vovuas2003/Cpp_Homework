da2993ce54e149275f06a5f0856b2e587419b063
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2023-01-02 17:17:55 +0300

    Update main.cpp

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -1,169 +1,150 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
 #include <cmath>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
 
-
-struct Ball
+class Draggable
 {
-    float radius;
-    float mass;
-    sf::Vector2f position;
-    sf::Vector2f velocity;
+private:
+    sf::RectangleShape mShape   {};
+    sf::RectangleShape mFON   {};
+    bool mIsDragged             {false};
+    sf::Vector2f mOffset        {0.0f, 0.0f};
+    int min_value {0};
+    int max_value {10};
+    float percent_value{0.0f};
+    sf::Font font;
+    sf::Text text;
+
+public:
+    Draggable() {};
+    Draggable(const sf::RectangleShape& shape) : mShape{shape} {}
+    Draggable(sf::Vector2f position, sf::Color color, int mi, int ma, float p) 
+    {
+        mShape.setPosition(position - sf::Vector2f{25, 25});
+        mFON.setPosition(position);
+        mShape.setSize({50, 100});
+        mFON.setSize({500, 50});
+        mShape.setFillColor(color);
+        mFON.setFillColor({200, 200, 200});
+        min_value = mi;
+        max_value = ma;
+        percent_value = p;
+        if (!font.loadFromFile("consola.ttf"))
+		std::cout << "Error loading font\n";
+	text.setFont(font);
+	text.setFillColor(color);
+        text.setCharacterSize(30);
+	text.setPosition(position + sf::Vector2f{550, 0});
+	text.setString(std::to_string((int) percent_value * max_value / 100));
+    }
+
+    bool onMousePressed(sf::Vector2f mousePosition)
+    {
+        if (mShape.getGlobalBounds().contains(mousePosition))
+        {
+            mIsDragged = true;
+            mOffset = mousePosition - mShape.getPosition();
+        }
+        else if (mFON.getGlobalBounds().contains(mousePosition))
+        {
+            mOffset = mousePosition - mShape.getPosition();
+            mShape.setPosition(sf::Vector2f{mousePosition.x - 25, mFON.getPosition().y - 25});
+        }
+        return mIsDragged;
+    }
+
+    void onMouseReleased()
+    {
+        mIsDragged = false;
+    }
+
+    void onMouseMoved(sf::Vector2f mousePosition)
+    {
+        if (mIsDragged)
+        {
+            mShape.setPosition(mousePosition - mOffset);
+        }
+    }
+
+    void setColor(sf::Color c)
+    {
+        mShape.setFillColor(c);
+    }
+
+    void setSize(sf::Vector2f sz)
+    {
+        mShape.setSize(sz);
+    }
+
+    void setPosition(sf::Vector2f sz)
+    {
+        mShape.setSize(sz);
+    }
+
+    void draw(sf::RenderWindow& rw) const
+    {
+        rw.draw(mFON);
+        rw.draw(mShape);
+        rw.draw(text);
+    }
 };
+
+
 
 int main()
 {
-    srand(time(0));
-    
-    const int width = 1000;
-    const int height = 600;
-    int n_balls = 0;
-    const int min_dist = 5;
-    const float G = 5;
-
-    // Шаг по времени
-    const float delta_t = 0.1;
-
-    // Создаём экземпляр класса окно
-    sf::RenderWindow window(sf::VideoMode(width, height), "My window");
-    // Задаём максимальное количество кадров в секунду
+    sf::RenderWindow window(sf::VideoMode(1300, 650), "Slider");
     window.setFramerateLimit(60);
 
-    // Так как sf::CircleShape занимает много памяти, создаём всего 1 экземпляр
-    sf::CircleShape circle(50.0f);
-    circle.setFillColor({200, 216, 200});
 
-    std::vector<Ball> balls;
-    balls.resize(n_balls);
-    for (int i = 0; i < n_balls; i++)
-    {
-        balls[i].radius = 4 + rand() % 8;
-        balls[i].mass = balls[i].radius * balls[i].radius;
-        if(rand() % 2) {
-            balls[i].mass *= -1;
-        }
-        balls[i].position = {(float)(rand() % width), (float)(rand() % height)};
-        balls[i].velocity = {(float)(rand() % 100 - 50), (float)(rand() % 100 - 50)};
-    }
-    int rjcnskm = 1;
+    Draggable ra {{50, 50}, sf::Color::White, 1, 100, 77};
+    Draggable r {{50, 200}, sf::Color::Red, 0, 255, 25};
+    Draggable g {{50, 350}, sf::Color::Green, 0, 255, 50};
+    Draggable b {{50, 500}, sf::Color::Blue, 0, 255, 75};
+
     while (window.isOpen())
     {
         sf::Event event;
-        window.setKeyRepeatEnabled(false);
         while (window.pollEvent(event))
         {
-            // В данном примере проверяем окно на закрытие
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
                 window.close();
+            
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+            {
+                sf::Vector2f mousePosition = window.mapPixelToCoords({event.mouseButton.x, event.mouseButton.y});
+                ra.onMousePressed(mousePosition);
+                r.onMousePressed(mousePosition);
+                g.onMousePressed(mousePosition);
+                b.onMousePressed(mousePosition);
             }
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::LShift) {
-                    rjcnskm = -1;
-                }
+
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
+            {
+                sf::Vector2f mousePosition = window.mapPixelToCoords({event.mouseButton.x, event.mouseButton.y});
+                ra.onMouseReleased();
+                r.onMouseReleased();
+                g.onMouseReleased();
+                b.onMouseReleased();
             }
-            if (event.type == sf::Event::KeyReleased) {
-                if (event.key.code == sf::Keyboard::LShift) {
-                    rjcnskm = 1;
-                }
-            }
-            if(event.type == sf::Event::MouseButtonPressed) {
-                if(event.mouseButton.button == sf::Mouse::Right) {
-                    Ball temp;
-                    temp.radius = 12;
-                    temp.mass = rjcnskm * temp.radius * temp.radius;
-                    temp.position = {(float)(event.mouseButton.x), (float)(event.mouseButton.y)};
-                    temp.velocity = {(float)(0), (float)(0)};
-                    balls.push_back(temp);
-                    n_balls++;
-                } else if(event.mouseButton.button == sf::Mouse::Left) {
-                    Ball temp;
-                    temp.radius = 6;
-                    temp.mass = rjcnskm * temp.radius * temp.radius;
-                    temp.position = {(float)(event.mouseButton.x), (float)(event.mouseButton.y)};
-                    temp.velocity = {(float)(0), (float)(0)};
-                    balls.push_back(temp);
-                    n_balls++;
-                }
+
+            if (event.type == sf::Event::MouseMoved)
+            {
+                sf::Vector2f mousePosition = window.mapPixelToCoords({event.mouseMove.x, event.mouseMove.y});
+                ra.onMouseMoved(mousePosition);
+                r.onMouseMoved(mousePosition);
+                g.onMouseMoved(mousePosition);
+                b.onMouseMoved(mousePosition);
             }
         }
-
-        // очистить скрытый холст черным цветом
+        
         window.clear(sf::Color::Black);
-
-        for (int i = 0; i < n_balls; i++)
-        {
-            balls[i].position += balls[i].velocity * delta_t;
-
-            /*
-            if (balls[i].position.x < 0)
-                balls[i].position.x += width;
-            if (balls[i].position.x > width)
-                balls[i].position.x -= width;
-
-            if (balls[i].position.y < 0)
-                balls[i].position.y += height;
-            if (balls[i].position.y > height)
-                balls[i].position.y -= height;
-            */
-            /*
-            if (balls[i].position.x < 0 || balls[i].position.x > width)
-                balls[i].velocity.x *= -1;
-            if (balls[i].position.y < 0 || balls[i].position.y > height)
-                balls[i].velocity.y *= -1;
-            */
-            if (balls[i].position.x - balls[i].radius < 0)
-                balls[i].velocity.x = fabs(balls[i].velocity.x);
-            if (balls[i].position.x + balls[i].radius > width)
-                balls[i].velocity.x = -1 * fabs(balls[i].velocity.x);
-            if (balls[i].position.y - balls[i].radius < 0)
-                balls[i].velocity.y = fabs(balls[i].velocity.y);
-            if (balls[i].position.y + balls[i].radius > height)
-                balls[i].velocity.y = -1 * fabs(balls[i].velocity.y);
-
-            for(int myi = 0; myi < n_balls; myi++) {
-                for(int myj = 0; myj < n_balls; myj++) {
-                    if(myi == myj) {
-                        continue;
-                    }
-                    float dist = std::sqrt((balls[myi].position.x - balls[myj].position.x) * (balls[myi].position.x - balls[myj].position.x) + (balls[myi].position.y - balls[myj].position.y) * (balls[myi].position.y - balls[myj].position.y));
-                    if(dist < min_dist) {
-                        continue;
-                    }
-                    auto path = balls[myi].position - balls[myj].position;
-                    if((balls[myi].mass * balls[myj].mass) < 0) {
-                        balls[myi].velocity -= (path * G * delta_t * fabs(balls[myj].mass)) / (dist * dist);
-                    } else {
-                        balls[myi].velocity += (path * G * delta_t * fabs(balls[myj].mass)) / (dist * dist);
-                    }
-                }
-            }
-            
-            // Используем 1 sf::CircleShape, чтобы нарисовать все шары
-            circle.setRadius(balls[i].radius);
-            if(balls[i].mass < 0) {
-                circle.setFillColor({0, 0, 255});
-            } else {
-                circle.setFillColor({255, 0, 0});
-            }
-            // setOrigin - задаёт центр объекта
-            // По умолчанию центр - в левом верхнем угле объекта
-            // Строка ниже устанавливает центр в центре шарика
-            // В дальнейшем функция, setPosition устанавливает положение шарика так, 
-            // чтобы его центр был в точке balls[i].position
-            circle.setOrigin(balls[i].radius, balls[i].radius);
-            circle.setPosition(balls[i].position);
-            
-            window.draw(circle);
-        }
-
-        // отображаем содержимое скрытого холста на экран
+        ra.draw(window);
+        r.draw(window);
+        g.draw(window);
+        b.draw(window);
         window.display();
     }
-
-    return 0;
 }
